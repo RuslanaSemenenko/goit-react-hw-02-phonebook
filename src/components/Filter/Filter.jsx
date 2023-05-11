574ae3dd3c14afc5eb0113b1240432f92e84a50d
Ruslana Semenenko <katotochkaaa@gmail.com>
2023-05-11 20:11:33 +0100

    homework

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,29 +1,3 @@
-// import css from './Filter.module.css';
-// import PropTypes from 'prop-types';
-
-// export const Filter = ({ filter, onChangeInput }) => {
-//   return (
-//     <>
-//       <label>
-//         Find contacts by name
-//         <br />
-//         <input
-//           className={css.input}
-//           onChange={onChangeInput}
-//           value={filter}
-//           type="text"
-//           name="filter"
-//         />
-//       </label>
-//     </>
-//   );
-// };
-
-// Filter.propTypes = {
-//   filter: PropTypes.string.isRequired,
-//   onChangeInput: PropTypes.func.isRequired,
-// };
-
 import React from 'react';
 import PropTypes from 'prop-types';
 
